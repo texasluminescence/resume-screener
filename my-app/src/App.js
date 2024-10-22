de7529383ece3051594d9d7a623d06c716b3e47b
WajeehJ <wajeehdoesstuff@gmail.com>
2024-10-21 20:40:46 -0500

    made landing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,69 @@
 import logo from './logo.svg';
-import './App.css';
+import './css_files/AfterEnteringResume.css';
+import './css_files/components.css';
+import './css_files/font.css';
+import './css_files/index.css';
+import './css_files/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="columnheader_logo-one">
+        <header class="header">
+          <div class="header_top-row">
+            <div class="header___navigation">
+              <ul class="header__menu">
+                <li>
+                  <a href="#" class="menu-item-resume-link">
+                    <p class="header_menu-item ui text size-textxs">Resume Scanner</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <p class="ui text size-textxs">Example Resumes</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <p class="ui text size-textxs">Community</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <p class="ui text size-textxs">Blogs</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <p class="ui text size-textxs">Tips</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <p class="ui text size-textxs">Contact</p>
+                  </a> 
+                </li>
+                <li>
+                  <button class="header____auth-button ui button gray_300 size-xs fill round">Sign in</button>
+                </li>
+                <li>
+                  <button class="header____auth-button-1 ui button blue_gray_900 size-xs fill round">Register</button>
+                </li>
+              </ul>
+            </div> 
+          </div>
+        </header>
+
+
+
+        
+        <div>
+          <div class="flex-col-center-center heroactions">
+            <h1 class="resume-scanner_title ui heading size-headingxs">Resume Scanner</h1>
+            <button class="button--upload ui button blue_gray_900 size-sm fill round">Upload File to Start Scan</button>
+          </div>
+        </div> 
+      </div>
+
   );
 }
 
